b679fca479243ed1fd3f5d38099c423d2a807919
Harshitha <VHARSHITHA.23@ichat.sp.edu.sg>
2024-05-03 01:38:42 +0800

    updated file

--- employee_info.py
+++ employee_info.py
@@ -1,4 +1,3 @@
-# Define a dictionary to store employee information
 employee_data = [
     {"name": "John", "age": 30, "department": "Sales", "salary": 50000},
     {"name": "Jane", "age": 25, "department": "Marketing", "salary": 60000},
@@ -21,25 +20,23 @@ def get_employees_by_age_range(age_lower_limit, age_upper_limit):
 def calculate_average_salary():
     total = 0
     average = 0
-
-    #add your implementation to calculate here
-
-
+    
+    for item in employee_data:
+      total = total+int(item["salary"])
+    average = total/len(employee_data)
     return average
 
 def get_employees_by_dept(department):
     result = []
-
-    # Add your implementation from here
-
-
+    for item in employee_data:
+      if (department==item['department']):
+            result.append(item)
     return result
 
 def display_all_records():
     print(("Name" + "\t" +"Age" +"\t" +"Department" +"\t" +"Salary" ).expandtabs(15))
     for item in employee_data:
-        print((item["name"] + "\t" + str(item["age"]) + "\t" + item["department"] + "\t" + str(item["salary"])).expandtabs(15))
-
+        print((item["name"] + "  " + str(item["age"]) + "\t" + item["department"] + "\t" + str(item["salary"])).expandtabs(15))
 
 def display_records(employee_info):
     print(("Name" + "\t" +"Age" +"\t" +"Department" +"\t" +"Salary" ).expandtabs(15))
